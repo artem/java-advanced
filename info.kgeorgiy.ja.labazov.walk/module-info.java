cd72bdb6d4e4ce5acdab97fc7fabc7c1764fc052
Artem Labazov <123321artyom@gmail.com>
2021-02-12 00:56:39 +0300

    Structural clusterfuck

---
+++ info.kgeorgiy.ja.labazov.walk/module-info.java
@@ -0,0 +1,7 @@
+module info.kgeorgiy.ja.labazov.walk {
+  requires transitive info.kgeorgiy.java.advanced.walk;
+
+  exports info.kgeorgiy.ja.labazov.walk;
+
+  opens info.kgeorgiy.ja.labazov.walk to junit;
+}
