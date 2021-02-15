5b3cd097f9e0cb399cf5653522c566d4eebe9ca1
Artem Labazov <123321artyom@gmail.com>
2021-02-15 15:48:15 +0300

    Update

--- modules/info.kgeorgiy.ja.labazov.walk/info/kgeorgiy/ja/labazov/walk/RecursiveWalk.java
+++ modules/info.kgeorgiy.ja.labazov.walk/info/kgeorgiy/ja/labazov/walk/RecursiveWalk.java
@@ -5,7 +5,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Objects;
 
-public class Walk {
+public class RecursiveWalk {
     private static class WalkVisitor extends SimpleFileVisitor<Path> {
         private final BufferedWriter out;
 
