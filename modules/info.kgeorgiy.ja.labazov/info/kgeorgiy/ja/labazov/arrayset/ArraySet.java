c739eb78d13ed8282d6607d07b76d96f5c046a7e
Artem Labazov <123321artyom@gmail.com>
2021-02-20 19:06:44 +0300

    Stub HW2 solution

---
+++ modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/arrayset/ArraySet.java
@@ -0,0 +1,100 @@
+package info.kgeorgiy.ja.labazov.arrayset;
+
+import java.util.*;
+
+public class ArraySet<E> extends AbstractSet<E> implements NavigableSet<E> {
+    @Override
+    public E lower(E e) {
+        return null;
+    }
+
+    @Override
+    public E floor(E e) {
+        return null;
+    }
+
+    @Override
+    public E ceiling(E e) {
+        return null;
+    }
+
+    @Override
+    public E higher(E e) {
+        return null;
+    }
+
+    @Override
+    public E pollFirst() {
+        return null;
+    }
+
+    @Override
+    public E pollLast() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> descendingSet() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> subSet(E e, boolean b, E e1, boolean b1) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> headSet(E e, boolean b) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> tailSet(E e, boolean b) {
+        return null;
+    }
+
+    @Override
+    public Comparator<? super E> comparator() {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> subSet(E e, E e1) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> headSet(E e) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> tailSet(E e) {
+        return null;
+    }
+
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E last() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
