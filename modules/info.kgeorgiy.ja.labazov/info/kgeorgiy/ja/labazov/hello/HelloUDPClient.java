21b535d47d79eec66275743708ee3067ef518258
Artem Labazov <123321artyom@gmail.com>
2021-05-22 22:09:21 +0300

    nikolai udp

--- modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPClient.java
+++ modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPClient.java
@@ -2,9 +2,81 @@ package info.kgeorgiy.ja.labazov.hello;
 
 import info.kgeorgiy.java.advanced.hello.HelloClient;
 
-public class HelloUDPClient implements HelloClient {
-    @Override
-    public void run(String host, int port, String prefix, int threads, int requests) {
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
+public class HelloUDPClient implements HelloClient {
+
+    public static void main(String[] args) {
+        if (args.length != 5) {
+            System.err.println("Usage: host port prefix threads requests");
+            return;
+        }
+        new HelloUDPClient().run(args[0], Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+    }
+
+    @Override
+    public void run(final String host, final int port, final String prefix, final int threads, final int requests) {
+        final ExecutorService executorService = Executors.newFixedThreadPool(threads);
+        final SocketAddress inetAddress = new InetSocketAddress(host, port);
+
+        IntStream.range(0, threads).forEach(index -> executorService.submit(() -> doRequest(index, requests, prefix, inetAddress)));
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(threads * requests * 5000L, TimeUnit.MILLISECONDS);
+        } catch (final InterruptedException ignored) {
+        }
+    }
+
+    private boolean receivePacket(final DatagramSocket datagramSocket, final DatagramPacket datagramPacket,
+                                  final String request, final byte[] responseStorage) {
+        try {
+            datagramPacket.setData(responseStorage, 0, responseStorage.length);
+            datagramSocket.receive(datagramPacket);
+            final String responseString = new String(
+                    datagramPacket.getData(),
+                    0,
+                    datagramPacket.getLength(),
+                    StandardCharsets.UTF_8);
+            if (responseString.contains(request)) {
+                System.out.println(responseString);
+                return true;
+            }
+        } catch (final IOException ignored) {
+        }
+        return false;
+    }
+
+    private void doRequest(final int threadIndex, final int requests, final String prefix, final SocketAddress inetAddress) {
+        try (final DatagramSocket datagramSocket = new DatagramSocket()) {
+            datagramSocket.setSoTimeout(500);
+            final byte[] responseStorage = new byte[datagramSocket.getReceiveBufferSize()];
+            for (int j = 0; j < requests; j++) {
+                final String request = (prefix + threadIndex + "_" + j);
+                final byte[] requestInBytes = request.getBytes(StandardCharsets.UTF_8);
+                System.out.println(request);
+                while (true) {
+                    try {
+                        final DatagramPacket datagramPacket = new DatagramPacket(
+                                requestInBytes,
+                                requestInBytes.length,
+                                inetAddress);
+
+                        datagramSocket.send(datagramPacket);
+                        if (receivePacket(datagramSocket, datagramPacket, request, responseStorage)) {
+                            break;
+                        }
+                    } catch (final IOException ignored) {
+                    }
+                }
+            }
+        } catch (final SocketException e) {
+            System.err.println("Socket problems: " + e.getMessage());
+        }
     }
 }
--- modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPServer.java
+++ modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPServer.java
@@ -2,14 +2,81 @@ package info.kgeorgiy.ja.labazov.hello;
 
 import info.kgeorgiy.java.advanced.hello.HelloServer;
 
-public class HelloUDPServer implements HelloServer {
-    @Override
-    public void start(int port, int threads) {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
+public class HelloUDPServer implements HelloServer {
+
+    private static final byte[] PREFIX_ANSWER = "Hello, ".getBytes(StandardCharsets.UTF_8);
+    private DatagramSocket udpSocket;
+    private ExecutorService executorService;
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: port threads");
+            return;
+        }
+        HelloServer server = new HelloUDPServer();
+        server.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+    }
+
+    @Override
+    public void start(final int port, final int threads) {
+        executorService = Executors.newFixedThreadPool(threads);
+        try {
+            udpSocket = new DatagramSocket(port);
+        } catch (final SocketException e) {
+            System.err.println("Failed to open socket on port " + port);
+            return;
+        }
+        IntStream.range(0, threads).forEach(index -> executorService.submit(this::listenSocket));
+    }
+
+    private void listenSocket() {
+        final byte[] byteBuffer;
+        try {
+            byteBuffer = new byte[udpSocket.getReceiveBufferSize()];
+        } catch (final SocketException e) {
+            System.err.println("Can't get size of socket's buffer: " + e.getMessage());
+            return;
+        }
+
+        while (!udpSocket.isClosed() && !Thread.currentThread().isInterrupted()) {
+            try {
+                final DatagramPacket receivePacket = new DatagramPacket(byteBuffer, 0, byteBuffer.length);
+                udpSocket.receive(receivePacket);
+                sendDatagram(udpSocket, receivePacket);
+            } catch (final IOException e) {
+                System.err.println("Receiving datagram failed: " + e.getMessage());
+            }
+        }
+    }
+
+    private void sendDatagram(final DatagramSocket datagramSocket, final DatagramPacket receivePacket) {
+        final byte[] responseByteArray = new byte[receivePacket.getLength() + PREFIX_ANSWER.length];
+        System.arraycopy(PREFIX_ANSWER, 0, responseByteArray, 0, PREFIX_ANSWER.length);
+        System.arraycopy(receivePacket.getData(), receivePacket.getOffset(),
+                responseByteArray, PREFIX_ANSWER.length, receivePacket.getLength());
+        receivePacket.setData(responseByteArray, 0, responseByteArray.length);
+        try {
+            datagramSocket.send(new DatagramPacket(responseByteArray, 0, responseByteArray.length,
+                    receivePacket.getAddress(), receivePacket.getPort()));
+        } catch (final IOException e) {
+            System.err.println("Failed to send packet: " + e.getMessage());
+        }
     }
 
     @Override
     public void close() {
-
+        if (udpSocket != null) {
+            udpSocket.close();
+        }
+        executorService.shutdownNow();
     }
 }
