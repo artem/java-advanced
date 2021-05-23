4fd357ed1e28c4bf57ddda46f0dd34fdb0c0d9bf
Artem Labazov <123321artyom@gmail.com>
2021-05-23 21:31:23 +0300

    udp start

--- modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPClient.java
+++ modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPClient.java
@@ -2,9 +2,27 @@ package info.kgeorgiy.ja.labazov.hello;
 
 import info.kgeorgiy.java.advanced.hello.HelloClient;
 
+import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class HelloUDPClient implements HelloClient {
+    public static void main(String[] args) {
+        if (args.length != 5) {
+            System.err.println("Usage: [host] [port] [prefix] [threads] [requests]");
+            return;
+        }
+        new HelloUDPClient().run(args[0], Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+    }
+
     @Override
     public void run(String host, int port, String prefix, int threads, int requests) {
+        ExecutorService workers = Executors.newFixedThreadPool(threads);
+        InetSocketAddress addr = new InetSocketAddress(host, port);
+    }
+
+    private static void sendRequest(String prefix, int threadId, int requestId, DatagramPacket packet) {
 
     }
 }
--- modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPServer.java
+++ modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/hello/HelloUDPServer.java
@@ -3,6 +3,15 @@ package info.kgeorgiy.ja.labazov.hello;
 import info.kgeorgiy.java.advanced.hello.HelloServer;
 
 public class HelloUDPServer implements HelloServer {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: [port] [threads]");
+            return;
+        }
+        HelloServer server = new HelloUDPServer();
+        server.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+    }
+
     @Override
     public void start(int port, int threads) {
 
