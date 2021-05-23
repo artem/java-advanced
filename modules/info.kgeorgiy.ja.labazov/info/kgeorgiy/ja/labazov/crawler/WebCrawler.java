bf410d205366766b4593b60aece3ba25cf08d62c
Artem Labazov <123321artyom@gmail.com>
2021-05-23 20:13:27 +0300

    remove

--- modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/crawler/WebCrawler.java
+++ modules/info.kgeorgiy.ja.labazov/info/kgeorgiy/ja/labazov/crawler/WebCrawler.java
@@ -1,26 +1,26 @@
 package info.kgeorgiy.ja.labazov.crawler;
 
-import info.kgeorgiy.java.advanced.crawler.*;
+import info.kgeorgiy.java.advanced.crawler.CachingDownloader;
+import info.kgeorgiy.java.advanced.crawler.Crawler;
+import info.kgeorgiy.java.advanced.crawler.Downloader;
+import info.kgeorgiy.java.advanced.crawler.Result;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.Predicate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class WebCrawler implements AdvancedCrawler {
+public class WebCrawler implements Crawler {
     private final Downloader downloader;
-    private final ExecutorService downloaders;
-    private final ExecutorService extractors;
+    private final int downloaders;
+    private final int extractors;
     private final int perHost;
-    private final Map<String, PageQueue> limitedQueues;
 
     public WebCrawler(Downloader downloader, int downloaders, int extractors, int perHost) {
         this.downloader = downloader;
-        this.downloaders = Executors.newFixedThreadPool(downloaders);
-        this.extractors = Executors.newFixedThreadPool(extractors);
+        this.downloaders = downloaders;
+        this.extractors = extractors;
         this.perHost = perHost;
-        limitedQueues = new ConcurrentHashMap<>();
     }
 
     private static void usage() {
@@ -39,7 +39,6 @@ public class WebCrawler implements AdvancedCrawler {
         }
 
         try {
-            /* Switch technology */
             switch (args.length) {
                 case 5:
                     perHost = Integer.parseUnsignedInt(args[4]);
@@ -59,123 +58,31 @@ public class WebCrawler implements AdvancedCrawler {
         }
 
         try {
-            Crawler crowler = new WebCrawler(new CachingDownloader(), downloads, extractors, perHost);
-            crowler.download(args[0], depth);
-            crowler.close();
+            new WebCrawler(new CachingDownloader(), downloads, extractors, perHost).download(args[0], depth);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private Set<String> downloadImpl() {
+        Set<String> downloaded = new HashSet<>();
+
+        return downloaded;
+    }
+
     @Override
     public Result download(String url, int depth) {
-        return download(url, depth, host -> true);
-    }
-
-    private void downloadUrl(int depth, Set<String> successful, Map<String, IOException> failed, Phaser phaser, Queue<String> newQueue, String url, Predicate<String> allowed) {
+        //List.copyOf();
         try {
-            final String hostName = URLUtils.getHost(url);
-            if (!allowed.test(hostName)) {
-                return;
-            }
-            final PageQueue pageQueue = limitedQueues.computeIfAbsent(hostName, host -> new PageQueue());
-
-            phaser.register();
-
-            pageQueue.submit(() -> {
-                try {
-                    Document res = downloader.download(url);
-                    if (phaser.getPhase() + 1 < depth) {
-                        extractUrl(successful, failed, phaser, newQueue, url, res);
-                    } else {
-                        successful.add(url);
-                    }
-                } catch (IOException e) {
-                    failed.put(url, e);
-                } finally {
-                    phaser.arriveAndDeregister();
-                    pageQueue.next();
-                }
-            });
-        } catch (MalformedURLException e) {
-            failed.put(url, e);
+            downloader.download(url);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    private void extractUrl(Set<String> successful, Map<String, IOException> failed, Phaser phaser, Queue<String> newQueue, String url, Document document) {
-        phaser.register();
-        extractors.submit(() -> {
-            try {
-                newQueue.addAll(document.extractLinks());
-                successful.add(url);
-            } catch (IOException e) {
-                failed.put(url, e);
-            } finally {
-                phaser.arriveAndDeregister();
-            }
-        });
+        return null;//ew Result();
     }
 
     @Override
     public void close() {
-        downloaders.shutdownNow();
-        extractors.shutdownNow();
-    }
 
-    @Override
-    public Result download(String url, int depth, List<String> hosts) {
-        final Set<String> allowed = new HashSet<>(hosts);
-        return download(url, depth, allowed::contains);
-    }
-
-    private Result download(String url, int depth, Predicate<String> allowed) {
-        final Set<String> visited = ConcurrentHashMap.newKeySet();
-        final Set<String> successful = ConcurrentHashMap.newKeySet();
-        final Map<String, IOException> failed = new ConcurrentHashMap<>();
-        Queue<String> queue = new ConcurrentLinkedQueue<>();
-        final Phaser phaser = new Phaser(1);
-
-        queue.add(url);
-
-        while (phaser.getPhase() < depth && !queue.isEmpty()) {
-            final Queue<String> newQueue = new ConcurrentLinkedQueue<>();
-
-            while (!queue.isEmpty()) {
-                String currentUrl = queue.poll();
-
-                if (!visited.add(currentUrl)) {
-                    continue;
-                }
-
-                downloadUrl(depth, successful, failed, phaser, newQueue, currentUrl, allowed);
-            }
-            phaser.arriveAndAwaitAdvance();
-            queue = newQueue;
-        }
-
-        return new Result(new ArrayList<>(successful), failed);
-    }
-
-    private class PageQueue {
-        private final Queue<Runnable> queue = new ArrayDeque<>();
-        private int busy;
-
-        public synchronized void submit(final Runnable runnable) {
-            if (busy < perHost) {
-                busy++;
-                downloaders.submit(runnable);
-            } else {
-                queue.add(runnable);
-            }
-        }
-
-        public synchronized void next() {
-            final Runnable runnable = queue.poll();
-            if (runnable == null) {
-                busy--;
-            } else {
-                downloaders.submit(runnable);
-            }
-        }
     }
 }
